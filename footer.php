e6b9f03573695a5bf361ca2c29da13268f6806fb
Hasini788 <manamperihasini@gmail.com>
2026-02-08 14:20:27 +0200

    Add files via upload
    fixed header and footer files

--- footer.php
+++ footer.php
@@ -1,7 +1,30 @@
-    <footer>
+ <style>
+    footer {
+        background-color: hsl(347, 90%, 56%);
+        color: white;
+        padding: 10px;
+        text-align: center;
+        position: absolute; 
+        bottom: 0;
+        width: 100%;
+    }
+       #main {
+        width: 300px;
+        height: 300px;
+        border: 1px solid black;
+        display: flex;
+    }
+
+    #main div {
+        flex: 1;
+    }
+
+</style>
+
+<footer>
         <!-- Your footer content goes here -->
          <p>2026 Hasini - Web Development project <br>
-         <a href = "hasini@hamk.fi">hasini.manamperi@student.hamk.fi</a>
+         <a href = "mailto:hasini.manamperi@student.hamk.fi">hasini.manamperi@student.hamk.fi</a>
 </p>
     </footer>
 </body>
--- header.php
+++ header.php
@@ -2,6 +2,22 @@
 <html>
 
 <head>
+    <meta chrset =UTF-8>
+<meta name ="viewport" content  ="width=device-width, initial-scale=1.0">
+
+<title><?php echo $title ;?></title>
+
+      <title>
+        <?php if (isset($title) && !empty($title))
+        {
+            echo $title;
+        }
+        else{
+            echo "DEFAULT TITLE";
+        }
+        ?>
+        </title>
+
     <title>Web development site by Hasini</title>
     <!-- Add your CSS, JavaScript, etc. here -->
      <style>
@@ -9,6 +25,19 @@
         th,td { padding: 10px 20px 30px 40px; }
         nav ul {list-style-type: none; padding: 0; background-color: #f4f4f4;}
         nav ul li {display: inline; margin-right: 20px;}
+
+         body {
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 0;
+            background-color: hsl(0, 0%, 100%);
+        }
+        header {
+            background-color: hsl(347, 90%, 56%);
+            color: white;
+            padding: 20px;
+            text-align: center;
+        }
         </style>
 </head>
 

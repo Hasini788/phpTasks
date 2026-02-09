4faf7b42102347e6e7f03baef05d68608f5cb016
Hasini788 <manamperihasini@gmail.com>
2026-02-09 06:07:26 +0200

    Add files via upload
    added last modified time

--- footer.php
+++ footer.php
@@ -27,6 +27,13 @@
          <p>2026 Hasini - Web Development project <br>
          <a href = "mailto:hasini.manamperi@student.hamk.fi">hasini.manamperi@student.hamk.fi</a>
 </p>
+<div class="mt-2" style="font-size: 0.8rem; opacity: 0.8;">
+<?php
+    $current_file = basename($_SERVER['PHP_SELF']);
+    $mod_time = filemtime($current_file);
+    echo "Last modified: " . date("F d Y H:i:s.", $mod_time);
+    ?>
+    </div>
     </footer>
 </body>
 </html>

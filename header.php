a608baddd6eab8c920719a14ac9b2b085590f3f6
Hasini788 <manamperihasini@gmail.com>
2026-02-09 10:53:02 +0200

    Add files via upload
    linked exercise 3 and 4

--- header.php
+++ header.php
@@ -48,7 +48,8 @@
          <nav>
             <ul>
                 <li><a href="ex1.php">Exercise 1</a></li>
-                <li><a href="ex2.php">Exercise 2</a></li>
-</ul>
+              <li><a href="ex3.php">Exercise 3</a></li>
+<li><a href="ex4.php">Exercise 4</a></li>
+            </ul>
 </nav>
     </header>

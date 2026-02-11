d8ad8662032c2bed9844e83f5d1241d7fc29bffe
Hasini788 <manamperihasini@gmail.com>
2026-02-11 10:17:31 +0200

    Add files via upload
    changed title positions

--- ex1.php
+++ ex1.php
@@ -1,5 +1,16 @@
 <?php include 'header.php'; ?>
 
+<div class="contenttitle" style="padding: 20px; text-align: center; background-color: #f8f9fa; border-bottom: 1px solid #ddd;">
+    <h2 style="color: #6d3e74;">Getting Started with PHP - Hasini</h2>
+</div>
+
+
+<div class="container mt-4 mb-4" style="padding: 20px; border-left: 5px solid #ff4d6d; background-color: #fff5f6;">
+    
+    <div style="display: flex; justify-content: center; margin-top: 20px;">
+       
+    </div>
+
 <h3>
 <?php
 echo "Hello world! My name is 'David'";
@@ -49,7 +60,7 @@ $g3 = 5;
 </tr>
 </table>
 
-<h2>Development environment screen shot</h2>
+<h2 style="margin-top: 50px;">Development environment screen shot</h2>
 <img src="ss.png" alt="development environment" width="800">
 
 <?php include 'footer.php'; ?>
